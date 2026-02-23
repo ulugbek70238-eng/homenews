2b73a3c1d7538250cdb862716265c85e71fc2b16
ulugbek70238-eng <ulugbek70238@gmail.com>
2026-02-23 22:56:19 +0500

    Обновил проект

--- arr/urls.py
+++ arr/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page),
-    path('category/<int:pk>', views.category_page),
-    path('category/<int:pk>/', views.theme_page, name='theme_page'),
+    path('', views.home),
+    path('categories/<int:pk>', views.home, name='home'),
+    path('themes/<int:pk>/', views.category_page, name='theme_page'),
 ]
--- arr/views.py
+++ arr/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from .models import Category, Theme
-# Create your views here.
+
 
 def home(request):
-    categories = Category.objects.all()
-    return render(request, 'home.html', {'categories': categories})
+    categ = Category.objects.all()
+    return render(request, 'home.html', {'categ': categ})
 
 def category_page(request, pk):
-    categori = Category.objects.all(id=pk)
     themes = Theme.objects.all()
-    return render(request, 'category.html', {'categori': categori, 'themes': themes})
-
+    return render(request, 'category.html', {'themes': themes})
