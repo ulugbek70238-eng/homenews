7e44c8e4b4a7f4e1b8e4c6a7a47cd789832b8fb8
ulugbek70238 <ulugbek70238@gmail.com>
2026-02-26 18:12:50 +0500

    рабочий код

--- arr/urls.py
+++ arr/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('categories/<int:pk>', views.home, name='home'),
-    path('themes/<int:pk>/', views.category_page, name='theme_page'),
+    path('categories/<int:pk>/', views.home, name='home'),
+    path('category_page/<int:pk>/', views.category_page, name='theme'),
+    path('theme_page/<int:pk>/', views.theme_page, name='theme_page'),
 ]
--- arr/views.py
+++ arr/views.py
@@ -7,5 +7,9 @@ def home(request):
     return render(request, 'home.html', {'categ': categ})
 
 def category_page(request, pk):
-    themes = Theme.objects.all()
-    return render(request, 'category.html', {'themes': themes})
+    themes = Theme.objects.filter(theme_category_id=pk)
+    return render(request, 'Category.html', {'themes': themes})
+
+def theme_page(request, pk):
+    themes1 = Theme.objects.get(id=pk)
+    return render(request, 'Themes.html', {'themes1': themes1})
