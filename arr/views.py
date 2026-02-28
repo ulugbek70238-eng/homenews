a3aafe9682aad883ba760315aa84159a7a897535
ulugbek70238 <ulugbek70238@gmail.com>
2026-03-01 00:52:39 +0500

    регистрация добавлена

--- arr/views.py
+++ arr/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Theme
-
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login, logout
+from .forms import RegForm
+from django.views import View
 
 def home(request):
     categ = Category.objects.all()
@@ -16,3 +19,30 @@ def category_page(request, pk):
 def theme_page(request, pk):
     themes1 = Theme.objects.get(id=pk)
     return render(request, 'Themes.html', {'themes1': themes1})
+
+def Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        form = RegForm
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = RegForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data('username')
+            email = form.cleaned_data('email')
+            password = form.cleaned_data('password1')
+
+
+            user = User.object.create_user(username=username,
+                                           email=email,
+                                           password=password)
+            user.save()
+            login(request, user)
+            return redirect('/')
+
+def search(request):
+    if request.method == 'POST':
+        search_product = request.POST.get['search_product']
