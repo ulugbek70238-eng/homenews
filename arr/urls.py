6fdfc4ce30f8fbfe8f388e9ff490d12bda8785c4
ulugbek70238-eng <ulugbek70238@gmail.com>
2026-02-28 13:15:28 +0500

    Описание изменений

--- arr/urls.py
+++ arr/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
@@ -7,3 +9,6 @@ urlpatterns = [
     path('category_page/<int:pk>/', views.category_page, name='theme'),
     path('theme_page/<int:pk>/', views.theme_page, name='theme_page'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- arr/views.py
+++ arr/views.py
@@ -4,11 +4,14 @@ from .models import Category, Theme
 
 def home(request):
     categ = Category.objects.all()
-    return render(request, 'home.html', {'categ': categ})
+    theme010 = Theme.objects.all()
+    return render(request, 'home.html', {'categ': categ, 'theme010': theme010})
+
 
 def category_page(request, pk):
+    categ1 = Category.objects.get(id=pk)
     themes = Theme.objects.filter(theme_category_id=pk)
-    return render(request, 'Category.html', {'themes': themes})
+    return render(request, 'category.html', {'categ1': categ1, 'themes': themes})
 
 def theme_page(request, pk):
     themes1 = Theme.objects.get(id=pk)
